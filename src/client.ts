502e4007702ba713fa844d4ded2f20187c2c6ea2
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2025-08-10 20:32:38 +0100

    feat: get card by set by number

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,8 @@
 import { AxiosError, CreateAxiosDefaults } from "axios";
 
-import type { Card, Error, List, Set } from "./types";
+import type { Card, Error, Language, List, Set } from "./types";
 import { AxiosClient } from "./utils/axios";
-import { SetNotFoundError } from "./utils/errors";
+import { CardNotFoundError, SetNotFoundError } from "./utils/errors";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface ScryfallClientConfig extends CreateAxiosDefaults {}
@@ -10,21 +10,42 @@ export interface ScryfallClientConfig extends CreateAxiosDefaults {}
 export class Scryfall {
   private readonly axiosClient: AxiosClient;
 
-  public constructor(config: ScryfallClientConfig = {}) {
+  constructor(config: ScryfallClientConfig = {}) {
     this.axiosClient = new AxiosClient(config);
   }
 
-  public readonly cards = {
+  readonly cards = {
     getRandomCard: async () => {
       try {
-        const data = await this.axiosClient.get<Card>("/cards/random");
-
-        return data;
+        return await this.axiosClient.get<Card>("/cards/random");
       } catch (error) {
         if (error instanceof AxiosError) {
           if (error.code === "EAI_AGAIN") throw error;
 
           const scryfallError = error.response?.data as Error;
+
+          throw new Error(scryfallError.details);
+        } else console.error(error);
+      }
+    },
+    getCardBySetByNumber: async (
+      set: string,
+      number: number,
+      lang?: Language,
+    ) => {
+      try {
+        const langPath = lang ? `/${lang}` : "";
+        return await this.axiosClient.get<Card>(
+          `/cards/${set}/${number}${langPath}`,
+        );
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          if (error.code === "EAI_AGAIN") throw error;
+
+          const scryfallError = error.response?.data as Error;
+
+          if (scryfallError.status === 404)
+            throw new CardNotFoundError(scryfallError.details);
 
           throw new Error(scryfallError.details);
         } else console.error(error);
@@ -59,22 +80,20 @@ export class Scryfall {
     },
   };
 
-  public readonly sets = {
+  readonly sets = {
     getSets: async () => {
       return await this.lists.getData<Set>("/sets");
     },
     getSet: async (code: string) => {
       try {
-        const data = await this.axiosClient.get<Set>(`/sets/${code}`);
-
-        return data;
+        return await this.axiosClient.get<Set>(`/sets/${code}`);
       } catch (error) {
         if (error instanceof AxiosError) {
           if (error.code === "EAI_AGAIN") throw error;
 
           const scryfallError = error.response?.data as Error;
 
-          if (error.status === 404)
+          if (scryfallError.status === 404)
             throw new SetNotFoundError(scryfallError.details);
 
           throw new Error(scryfallError.details);
--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -3,6 +3,15 @@ export class SetNotFoundError extends Error {
   constructor(message: string) {
     super(message);
     this.name = "SetNotFoundError";
-    this.code = "1001";
+    this.code = "1000";
+  }
+}
+
+export class CardNotFoundError extends Error {
+  public code: string;
+  constructor(message: string) {
+    super(message);
+    this.name = "CardNotFoundError";
+    this.code = "1100";
   }
 }
